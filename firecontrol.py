dd22cbafdf46fde8b8fb7ed70939382afd5b26fb
EmilRLee <68071089+EmilRLee@users.noreply.github.com>
2020-08-16 19:48:38 -0400

    Update firecontrol.py

--- firecontrol.py
+++ firecontrol.py
@@ -1,12 +1,27 @@
 #!/usr/bin/bash
 
 import os, time, socket, yaml, argparse,sys, subprocess, pickle
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, abort, redirect, url_for
 
 
 PORT = 5050
 #HOST = '192.168.5.24'
 app = Flask(__name__,template_folder='ui/templates', static_folder='ui/static')
+
+def agentinfo(HOST,agent):
+    print(f"agent ========= {agent}")
+    firesocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    firesocket.connect((HOST,PORT))
+    firesocket.sendall(b'$agent-table$')
+    #after connect send agent ip to get its iptables
+    time.sleep(10)
+    firesocket.sendall(bytes(str(agent),'UTF-8'))
+    
+    with open("{}.iptable".format(agent), "wb") as iptable:
+        agentconfig = firesocket.recv(65535)
+        iptable.write(agentconfig)
+
+
 
 def webvars(HOST):
     firesocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -75,8 +90,50 @@ def web_home():
 
     return render_template("home.html", host=HOST, activeCount=activeCount, active_agents=active_agents, inactiveCount=inactiveCount, inactive_agents=inactive_agents, serverName=serverName, AgentCount=AgentCount, fire_agents=fire_agents, UpTime=UpTime)
 
-@app.route("/listagents")
-def web():
+@app.route("/agents/<agent>/", methods=['GET','POST'])
+def agent(agent):
+    
+    agentinfo(app.config['HOST'],agent)
+
+    with open("{}.iptable".format(agent), "r") as file:
+        agentconfig = file.readlines()
+    
+    if request.method == "POST":
+		
+        updatedconfig = request.form['config']
+
+        with open("{}.iptable".format(agent), "w+") as file:
+            file.write(updatedconfig)
+            print("UPDATED TABLE")
+        file.close
+        #flash(attempted_username)
+        #flash(attempted_password)
+
+        firesocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        firesocket.connect((app.config['HOST'],PORT))
+        print(f"attempting to connect to firecontroller at {app.config['HOST']}")
+        firesocket.sendall(b"$push-config$")
+        status = firesocket.recv(1024)
+        print("fire_server ->\n" + status.decode())
+        #need to send the name of file first then file.
+        agntable = agent + ".iptable"
+        firesocket.sendall(bytes(agntable, 'UTF-8'))
+        firesocket.sendall(bytes(updatedconfig,'UTF-8'))
+
+            
+        print("file sent")
+
+        status = firesocket.recv(1024)
+        print("fire_server ->\n" + status.decode())
+        firesocket.close()
+        print(updatedconfig)
+        return redirect(url_for('agent', agent=agent))
+    else:			
+        return render_template("agentinfo.html", agent=agent, agentconfig=agentconfig)
+
+
+@app.route("/agents")
+def listagents():
     #### xxx Needs ATTENTION vvvvvvvvvvvvvv need to change to platform agnostic
     agents = subprocess.run("c:/Users/DracoThaKing/AppData/Local/Programs/Python/Python38-32/python.exe firecontrol.py -listagents", check=True, stdout=subprocess.PIPE , universal_newlines=True)
 
@@ -107,14 +164,20 @@ class firecontrol:
         print("fire_server -> " + status.decode())
         #send agent ip to retrieved its configs
         firesocket.sendall(bytes(str(agent), 'UTF-8'))
-        agent_config = firesocket.recv(65535)
-        print("fire_server -> " + agent_config.decode())
-        with open("{}.yaml".format(agent), 'w+') as file:
-            file.write(agent_config.decode())
-        agent_config = firesocket.recv(65535)
-        print("fire_server ->" + agent_config.decode())
-        with open("{}.iptable".format(agent), 'w+') as file1:
-            file1.write(agent_config.decode())
+        config_status = firesocket.recv(65535)
+        print("fire_server -> " + config_status.decode())
+        if config_status.decode() !="agent not registered":
+            
+            yaml_config = firesocket.recv(65535)
+            with open("{}.yaml".format(agent), 'w+') as file:
+                file.write(yaml_config.decode())
+            print("fire_server ->" + yaml_config.decode())
+        
+            iptable_config = firesocket.recv(65535)
+            with open("{}.iptable".format(agent), 'w+') as file1:
+                file1.write(iptable_config.decode())
+            print("fire_server ->" + iptable_config.decode())
+                   
 
     def update(HOST,agent):
         firesocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -144,20 +207,24 @@ class firecontrol:
         with open("{}".format(config), "rb") as file:
             bytestosend = file.read(65535)
             firesocket.send(bytestosend)
+            print(bytestosend)
         print("file sent")
 
         status = firesocket.recv(1024)
         print("fire_server ->\n" + status.decode())
+        agent_status = firesocket.recv(1024)
+        print(f"fire_server -> {agent_status.decode()}") 
+        firesocket.close()
 
 
 def main():
 
     parser = argparse.ArgumentParser(description='firewall-cmd/Netfilter firewall configration')
     parser.add_argument("-listagents", help="lists registered agents", action="store_true")
-    parser.add_argument("-getconfig", help="lists registered agents", action="store")
-    parser.add_argument("-pushconfig", help="configuration file to send", action="store")
+    parser.add_argument("-getconfig", help="request registered agents config file", action="store")
+    parser.add_argument("-pushconfig", help="push new configuration file to agent", action="store")
     parser.add_argument("-server_stop", help="stops the fireserver", action="store_true")
-    parser.add_argument("--web", help="initiates the web app interface. The app provides a graphical ui for configuring firewalls", action="store_true")
+    parser.add_argument("--web", help="initiates A web graphical interface. The app provides a graphical ui for configuring firewalls", action="store_true")
     parser.add_argument("-server", help="supply the fireserver IP", action="store")
     args = parser.parse_args()
     
