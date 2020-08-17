cee4a52777aab9f51ce55a3ef5a6ad64d9ee0ac6
EmilRLee <68071089+EmilRLee@users.noreply.github.com>
2020-08-17 01:56:16 -0400

    Update fire_agent.py
    added encryption to communications using certificates

--- fire_agent.py
+++ fire_agent.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3.6
 
-import socket, sys, struct, os, time, pickle, argparse, netifaces, threading
+import socket, sys, struct, os, time, pickle, argparse, netifaces, threading, ssl
 
 
 PORT = 5050
@@ -15,29 +15,32 @@ class fireagent:
 		for key, value in inet_addr.items():
 			if key == 'addr':
 				return bytes(value,'UTF-8')	
-
 		return ipaddress
 
 	def agentInit(SERVER,interface):
 		pollsig = b"$agent-poll$"
 		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 		print(f"attempting to connect to firecontroller at {SERVER}")
-		s.connect((SERVER,PORT))
+		context = ssl.create_default_context(ssl.Purpose.SERVER_AUTH)
+		context.verify_mode = ssl.CERT_REQUIRED
+		context.load_verify_locations("./certs/cacert.crt")
+		conn = context.wrap_socket(s, server_hostname="FireStorm", server_side=False)
+		conn.connect((SERVER,PORT))
 		print("connection successful")
-		s.sendall(pollsig)
+		conn.sendall(pollsig)
 		print("polling firecontroller")
-		data = s.recv(1024)
+		data = conn.recv(1024)
 		if not data:
 			print("polling failed, cannot find firecontroller")
 		else:
 			
 			print("sending hostinfo ipaddress")
 			hostip = fireagent.getHostInfo(interface)
-			s.sendall(hostip)
+			conn.sendall(hostip)
 			print("firecontroller msg ->" + repr(data.decode()))
-			status = s.recv(1024)
+			status = conn.recv(1024)
 			print("firecontroller msg ->" + repr(status.decode()))			
-		s.close()
+		conn.close()
 		time.sleep(1)
 		t = threading.Timer(1,fireagent.get_conf,[SERVER,interface])
 		t.setDaemon(True)
@@ -48,44 +51,52 @@ class fireagent:
 			hostip = fireagent.getHostInfo(interface)
 			s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 			try:
-				s.connect((SERVER,PORT))
-				s.sendall(b"$agent-config$")
-				s.sendall(hostip)
-				status = s.recv(1024)
+				context = ssl.create_default_context(ssl.Purpose.SERVER_AUTH)
+				context.verify_mode = ssl.CERT_REQUIRED
+				context.load_verify_locations("./certs/cacert.crt")
+				conn = context.wrap_socket(s, server_hostname="FireStorm", server_side=False)
+				conn.connect((SERVER,PORT))
+				conn.sendall(b"$agent-config$")
+				conn.sendall(hostip)
+				status = conn.recv(1024)
 				if status.decode() == "agent has configuration on file sending it now.":
-					conf = s.recv(65535)
+					conf = conn.recv(65535)
 					with open("agent.iptable", "wb") as rule:
 						rule.write(conf)
 					
 			
 					restore = os.popen("iptables-restore agent.iptable").read()
-					s.sendall(bytes(restore, 'UTF-8'))
-					s.close()
+					conn.sendall(bytes(restore, 'UTF-8'))
+					conn.close()
 				else:
 					os.system("iptables-save > agent.iptable")
 					time.sleep(1)
 					with open("./agent.iptable", "rb") as file:
 						conf = file.read(65535)
 						print(conf.decode("UTF-8"))
-						s.send(conf)
+						conn.send(conf)
 			except:
 				continue
 			time.sleep(60)
 	#push agents current configurations to the server
 	def push_conf(SERVER):
 		s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-		s.connect((SERVER,PORT))
-		s.sendall(b"$agent-push$")
-		stat = s.recv(1024)
+		context = ssl.create_default_context(ssl.Purpose.SERVER_AUTH)
+		context.verify_mode = ssl.CERT_REQUIRED
+		context.load_verify_locations("./certs/cacert.crt")
+		conn = context.wrap_socket(s, server_hostname="FireStorm", server_side=False)
+		conn.connect((SERVER,PORT))
+		conn.sendall(b"$agent-push$")
+		stat = conn.recv(1024)
 		print(stat.decode())
-		s.sendall(hostip)
+		conn.sendall(hostip)
 		
 		os.popen("iptables-save > agent.iptable")
 		with open("agent.iptable", "rb") as file:
 			conf = file.read(65535)
 			print(conf.decode("UTF-8"))
-			s.send(conf)
-		stat = s.recv(1024)
+			conn.send(conf)
+		stat = conn.recv(1024)
 		print(stat.decode())
 	def agentPoll(SERVER,interface):
 		while True:
@@ -93,21 +104,25 @@ class fireagent:
 			s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 			print(f"attempting to connect to firecontroller at {SERVER}")
 			try:
-				s.connect((SERVER,PORT))
+				context = ssl.create_default_context(ssl.Purpose.SERVER_AUTH)
+				context.verify_mode = ssl.CERT_REQUIRED
+				context.load_verify_locations("./certs/cacert.crt")
+				conn = context.wrap_socket(s, server_hostname="FireStorm", server_side=False)
+				conn.connect((SERVER,PORT))
 				print("connection successful")
-				s.sendall(pollsig)
+				conn.sendall(pollsig)
 				print("polling firecontroller")
-				data = s.recv(1024)
+				data = conn.recv(1024)
 				if not data:
 					print("polling failed, cannot find firecontroller")
 				else:
 				
 					print("sending hostinfo ipaddress")
-					s.sendall(fireagent.getHostInfo(interface))
+					conn.sendall(fireagent.getHostInfo(interface))
 					print("firecontroller msg ->" + repr(data.decode()))
-					status = s.recv(1024)
+					status = conn.recv(1024)
 					print("firecontroller msg ->" + repr(status.decode()))
-				s.close()
+				conn.close()
 			except:
 				print(socket.error)
 				continue			
@@ -120,26 +135,34 @@ class fireagent:
 		agentsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 		bind = agentsocket.bind((hostip,5050))
 		agentsocket.listen()
-		
+		context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
+		context.load_cert_chain(certfile="./certs/agent_cacert.crt", keyfile="./certs/agent_cert.pem", password="mypass")  # 1. key, 2. cert, 3. intermediates
+		#context.options |= ssl.OP_NO_TLSv1 | ssl.OP_NO_TLSv1_1  # optional
+		context.set_ciphers('EECDH+AESGCM:EDH+AESGCM:AES256+EECDH:AES256+EDH')
+
 		while True:
 			print("waiting on server commands")
-			(serversocket, address) = agentsocket.accept()
-			print("connection recieved from " + address[0])
-			auth = serversocket.recv(1024)
-			configtype = serversocket.recv(1024)
-			print("CONFIG TYPE" + configtype.decode())
-			if auth.startswith(b"$server-auth$"):
-		
-				if configtype == b'iptable':	
-					configfile = serversocket.recv(65535)
-					with open("agent.iptable", "w") as file:
-						file.write(configfile.decode().replace("\r\n","\n"))
-						#print(file.read(65535))
-					print('WROTE TO FILE')
-					command = serversocket.recv(1024)
-					print("command recieved: "  + command.decode())
-					os.popen(command.decode())
-				serversocket.close()
+			try:
+				(conn, address) = agentsocket.accept()
+				serversocket = self.context.wrap_socket(conn, server_side=True)
+				print("connection recieved from " + address[0])
+				auth = serversocket.recv(1024)
+				configtype = serversocket.recv(1024)
+				print("CONFIG TYPE" + configtype.decode())
+				if auth.startswith(b"$server-auth$"):
+			
+					if configtype == b'iptable':	
+						configfile = serversocket.recv(65535)
+						with open("agent.iptable", "w") as file:
+							file.write(configfile.decode().replace("\r\n","\n"))
+							#print(file.read(65535))
+						print('WROTE TO FILE')
+						command = serversocket.recv(1024)
+						print("command recieved: "  + command.decode())
+						os.popen(command.decode())
+					serversocket.close()
+			except:
+				continue
 				
 
 def main():
